3c0b0f67ca876de29842868390d174f01c56fc09
Monster0506 <tjraklovits@gmail.com>
2026-02-06 20:54:31 -0500

    update

--- server.py
+++ server.py
@@ -76,4 +76,4 @@ def static_files(path):
 
 if __name__ == "__main__":
     print("Code Mosaic server running at http://localhost:5000")
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=3000)
